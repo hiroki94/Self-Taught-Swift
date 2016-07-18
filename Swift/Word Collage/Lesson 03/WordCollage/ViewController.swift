e17f32bcf7aefb8a9875a2d1558dcea6475134b8
Hiroki Hayashi <hiroki_hayashi@mm-mbp15-02m.ad.marakovits.net>
2016-07-18 16:49:38 -0400

    Day 1

---
+++ Swift/Word Collage/Lesson 03/WordCollage/ViewController.swift
@@ -0,0 +1,28 @@
+/*
+
+This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike
+4.0 International License, by Yong Bakos.
+
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func changeBackgroundColor(sender: AnyObject) {
+        view.backgroundColor = UIColor.purpleColor()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
